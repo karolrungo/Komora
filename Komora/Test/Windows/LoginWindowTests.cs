e4997aaf734c78464b4cf2f9baa1302eb704966e
rungus2 <karolrungo@gmail.com>
2014-12-10 10:00:42 +0100

    refactor; todo-> open menu on event

---
+++ Komora/Test/Windows/LoginWindowTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using Komora;
+
+namespace Komora.Test.Windows
+{
+    [TestFixture]
+    public class LoginWindowTests
+    {
+        [TestCase]
+        public void WhenUserCredentialsAreProperLogerShouldRaiseLoginSuccesEvent()
+        {
+            var logerMock = new Mock<Classes.Loger.Loger>();
+            logerMock.Setup(m => m.validateUser(It.IsAny<string>(), It.IsAny<string>())).Raises(m => m.loginSucces += null, EventArgs.Empty);
+
+            Komora.Windows.LoginWindow loginWindow = new 
+        }
+    }
+}
