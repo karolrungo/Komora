6b5369bd21e6740627493cf636bb628140694583
rungus2 <karolrungo@gmail.com>
2014-12-11 09:56:23 +0100

    latest wersion- thursday morning at work

---
+++ Komora/DataTypes/MeasurementSamples.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komora.DataTypes
+{
+    public class MeasurementSamples<T>
+    {
+        List<Tuple<T, T>> samples;
+
+        public MeasurementSamples()
+        {
+            samples = new List<Tuple<T, T>>();
+        }
+
+        public MeasurementSamples(MeasurementSamples<T> measurementSamples)
+        {
+            this.samples = measurementSamples.samples;
+        }
+
+        public MeasurementSamples(List<T> x, List<T> y): this()
+        {
+            for (int i = 0; i < x.Count; ++i)
+            {
+                samples.Add(new Tuple<T, T>(x[i], y[i]));
+            }
+        }
+
+        public MeasurementSamples(List<Tuple<T, T>> list)
+        {
+            this.samples = list;
+        }
+
+        public List<Tuple<T, T>> getSamples()
+        {
+            return samples;
+        }
+
+        public int getCount()
+        {
+            return samples.Count;
+        }
+    }
+}
