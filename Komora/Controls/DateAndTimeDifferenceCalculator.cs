2ebaee65aa7a5d713ede2ea8a2a2176d9c2e004c
rungokarol <karolrungo@gmail.com>
2015-02-23 21:15:20 +0100

    date time calculator control added

---
+++ Komora/Controls/DateAndTimeDifferenceCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Komora.Controls
+{
+    public partial class DateAndTimeDifferenceCalculator : UserControl
+    {
+        public DateAndTimeDifferenceCalculator()
+        {
+            InitializeComponent();
+            labelResult.Text = "";
+        }
+
+        private void calculateTimeDifference()
+        {
+            DateTime start = dateAndTimePickerStart.getDateAndTime();
+            DateTime end = dateAndTimePickerEnd.getDateAndTime();
+            TimeSpan diff  = start.Subtract(end.Date);
+
+            labelResult.Text = formatOutputString(diff);
+        }
+        private string formatOutputString(TimeSpan result)
+        {
+            StringBuilder dateDiffString = new StringBuilder();
+            dateDiffString.Append("Difference beetween dates is: ").Append(Environment.NewLine);
+            dateDiffString.Append(result.Days).Append(" days ");
+            dateDiffString.Append(result.Minutes).Append(" minutes ");
+            dateDiffString.Append(result.Seconds).Append(" seconds ");
+            return dateDiffString.ToString();
+        }
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            calculateTimeDifference();
+        }
+    }
+}
---
+++ Komora/Windows/MatlabWndow.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace Komora.Windows
+{
+    partial class MatlabWndow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialogResults";
+            // 
+            // MatlabWndow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 141);
+            this.Name = "MatlabWndow";
+            this.Text = "MatlabWndow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
