fa973f458cac86063b6e4fa9a8c9866d335f9a29
rungokarol <karolrungo@gmail.com>
2015-02-24 21:23:09 +0100

    chamberData class added with tests

---
+++ Komora/Test/Chamber/ChamberDataTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Komora.Classes.Chamber;
+
+namespace Komora.Test.Chamber
+{
+    [TestFixture]
+    class ChamberDataTests
+    {
+        [Test]
+        public void numberPropertyThrowsExceptionWhenCanNotParseStringToInt()
+        {
+            ChamberData chamberData = new ChamberData();
+            string numberString = "not number string";
+
+            Assert.Throws(typeof(FormatException),
+                           delegate { chamberData.Number = Int32.Parse(numberString); });
+        }
+    }
+}
